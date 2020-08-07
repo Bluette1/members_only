1dfeb0180ea210f0c93bfd2a24b5ebcb111c1c40
Bluette1 <marylene.sawyer@gmail.com>
2020-08-07 12:18:32 +0200

    Add username field to sign in form, index view, added migration

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,11 +29,11 @@ class PostsController < ApplicationController
     
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
+        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: posts_path }
       else
         format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.json { render json: posts_path.errors, status: :unprocessable_entity }
       end
     end
   end
